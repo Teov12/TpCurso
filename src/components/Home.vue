9047c046deaa3392052cc4cd2def6e026987694d
Teo Camusso <teocamusso29@gmail.com>
2022-12-14 08:28:45 -0300

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "./Header.vue";
+import Layout from "./Layout.vue";
+</script>
+
+<template>
+<Layout>
+  <template #header>
+    <Header></Header>
+  </template>
+</Layout>
+</template>
+
+<style>
+
+</style>
